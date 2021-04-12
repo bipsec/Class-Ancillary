37545337ee96714b1451983ce20de362189cd892
Biplab Kumar Sarkar <66509972+bipsec@users.noreply.github.com>
2021-04-12 20:07:29 +0600

    initial commit for Class-Ancillary

---
+++ app/Http/Controllers/Auth/AdminregisterController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Admin;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
+use Auth;
+
+class AdminregisterController extends Controller
+{
+    
+   
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+    public function showRegistrationForm()
+    {
+        return view('admin-register');
+    }
+    public function register(Request $request)
+    {
+         $this->validate($request, [
+          'name'          => 'required',
+          'email'         => 'required',
+          'password'      => 'required'
+        ]);
+        // store in the database
+        $admins = new Admin;
+        $admins->name = $request->name;
+        $admins->email = $request->email;
+        $admins->password=bcrypt($request->password);
+        $admins->save();
+        return redirect()->route('admin.login');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return Admin::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+      protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        //
+    }
+}
